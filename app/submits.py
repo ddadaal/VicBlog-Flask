5592dcb043115f6b12094c3fd75175ed322c481a
Chen Junda <smallda@outlook.com>
2016-11-24 22:47:16 +0800

    building compose page

--- app/submits.py
+++ app/submits.py
@@ -3,13 +3,13 @@ from app import database, user
 from markdown2 import Markdown
 
 
-class BaseSubmit(object):
+class BaseSubmit():
 
     def execute(self):
         pass
     
     def construct_payload(self):
-        pass
+        return self.__dict__
 
     def acquire_time(self):
         return datetime.datetime.strftime(datetime.datetime.utcnow(),"%a, %d %b %Y %H:%M:%S GMT")
@@ -19,15 +19,7 @@ class RegisterSubmit(BaseSubmit):
         self.username=RegisterPackage["username"]
         self.password=RegisterPackage['password']
         self.role="user"
-
-    def construct_payload(self):
-        payload={
-            'username':self.username, 
-            'password':self.password,
-            'register_time':self.acquire_time(),
-            'role':self.role, 
-        }
-        return payload
+        self.register_time=self.acquire_time()
     
     def exist(self):
         query={"username":self.username}
@@ -69,43 +61,7 @@ class ArticleSubmit(BaseSubmit):
         self.submit_time=self.acquire_time()
         self.title=ArticlePackage["title"]
         self.categories=ArticlePackage["categories"]
-
-    def construct_payload(self):
-        payload={
-            "username": self.user.username,
-            "submit_time": self.submit_time, 
-            "content": self.content,
-            "title": self.title,  
-        }
-        return payload
     
     def execute(self):
         collection="article"
         return database.insert(collection,self.construct_payload())
-
-# class RegisterSubmit(BaseSubmit):
-#     def __init__(self, RegisterPackage):
-#         self.username=RegisterPackage['username']
-#         self.password=RegisterPackage['password']
-#         self.role=RegisterPackage["role"]
-#         self.submit_url="http://127.0.0.1:5001/users"
-    
-#     def construct_payload(self):
-#         payload={
-#             'username':self.username, 
-#             'password':self.password,
-#             'role':"user", 
-#             'time':self.acquire_time(), 
-#         }
-#         return payload
-
-#     def execute(self):
-#         payload=self.construct_payload()
-#         if not self.exist():
-#             return "User existing. Change the username "
-#         else:
-#             return requests.post(self.submit_url,payload).text
-    
-#     def exist(self):
-#         url=self.submit_url+"/"+self.username
-#         return requests.get(url).status_code==404
